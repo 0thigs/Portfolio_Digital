69c8f4f7d8bf061b6e3a13a924f4422cebe4d0b4
0thigs <machinimavlogs@gmail.com>
2024-05-10 09:07:50 -0300

    Config and Folder Modify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./api/templates/**/*.{html,js}",
+    "./api/static/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.{html,js}",
+  ],
   theme: {
     extend: {
       fontFamily: {
