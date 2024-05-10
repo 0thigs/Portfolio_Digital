ec8363073c98b1a00a3df112b20f7d6599830544
0thigs <machinimavlogs@gmail.com>
2024-05-10 09:08:09 -0300

    Python Script main

---
+++ api/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for
+
+dir_template_folder = 'templates/'
+dir_static_folder = "static"
+
+app = Flask(__name__, template_folder=dir_template_folder, static_folder=dir_static_folder)
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
