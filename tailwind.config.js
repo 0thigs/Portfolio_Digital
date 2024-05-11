2c6588e53c80b00471169f09d51db70de1b59c45
0thigs <machinimavlogs@gmail.com>
2024-05-11 01:55:25 -0300

    🖥config: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,12 @@ module.exports = {
     extend: {
       fontFamily: {
         montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        defaultPurple: "#4A1D96",
+        strokePurple: "#AC94FA",
       }
     }
   },
   plugins: [],
 }
-
