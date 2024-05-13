c8e4040d1015e6d8d89faa7fae4769cb5951001f
0thigs <machinimavlogs@gmail.com>
2024-05-12 21:40:17 -0300

    🖥script: routes and others scripts

--- api/main.py
+++ api/main.py
@@ -9,5 +9,13 @@ app = Flask(__name__, template_folder=dir_template_folder, static_folder=dir_sta
 def homepage():
     return render_template("homepage.html")
 
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
