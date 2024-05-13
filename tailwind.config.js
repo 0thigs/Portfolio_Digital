c6b309cffbdddb0effbcbde37bccfe2065884e44
0thigs <machinimavlogs@gmail.com>
2024-05-12 21:40:02 -0300

    🔧config: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./api/templates/**/*.{html,js}",
-    "./api/static/**/*.{html,js}",
-    "./node_modules/flowbite/**/*.{html,js}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        montserrat: ['Montserrat', 'sans-serif'],
-      },
-      colors: {
-        defaultPurple: "#4A1D96",
-        strokePurple: "#AC94FA",
-      }
-    }
-  },
-  plugins: [],
-}
+	content: [
+		"./api/templates/**/*.{html,js}",
+		"./api/static/**/*.{html,js}",
+		"./node_modules/flowbite/**/*.{html,js}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				montserrat: ["Montserrat", "sans-serif"],
+			},
+			colors: {
+				defaultPurple: "#4A1D96",
+				strokePurple: "#AC94FA",
+			},
+			animation: {
+				"infinite-scroll": "infinite-scroll 35s linear infinite",
+			},
+			keyframes: {
+				"infinite-scroll": {
+					from: { transform: "translateX(0)" },
+					to: { transform: "translateX(-100%)" },
+				},
+			},
+		},
+		plugins: [],
+	},
+};
