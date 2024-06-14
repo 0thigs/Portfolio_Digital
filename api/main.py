993ecc38c149adf4ad0f2b81d1a01166f2649cd1
0thigs <machinimavlogs@gmail.com>
2024-06-14 18:50:13 -0300

    💻scripts: send backend information

--- api/main.py
+++ api/main.py
@@ -11,7 +11,39 @@ def homepage():
 
 @app.route("/projects")
 def projects():
-    return render_template("projects.html")
+    allProjects = [
+    {
+        "title": "Youtube Music Clone",
+        "image": url_for('static', filename='/images/card(4).png'),
+        "link": "https://github.com/0thigs/Youtube_music"
+    },
+    {
+        "title": "StarDust",
+        "image": url_for('static', filename='/images/card(5).png'),
+        "link": "https://github.com/0thigs/StarDust",
+    },
+    {
+        "title": "Pokedéx",
+        "image": url_for('static', filename='/images/card(6).png'),
+        "link": "https://github.com/0thigs/Pokedex-App",
+    },
+    {
+        "title": "ChatApp",
+        "image": url_for('static', filename='/images/card(1).png'),
+        "link": "https://github.com/0thigs/ChatApp",
+    },
+    {
+        "title": "DevLinks",
+        "image": url_for('static', filename='/images/card(2).png'),
+        "link": "https://github.com/0thigs/DevLinks",
+    },
+    {
+        "title": "Github Finder",
+        "image": url_for('static', filename='/images/card(3).png'),
+        "link": "https://github.com/0thigs/Github-Profile-Finder",
+    }
+    ]
+    return render_template("projects.html", allProjects=allProjects)
 
 @app.route("/contact")
 def contact():
