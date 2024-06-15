9f0a6c161124d61ac69de2ddfbaa4c03a2e188c5
0thigs <machinimavlogs@gmail.com>
2024-06-14 22:44:44 -0300

    🖥scripts: update main

--- api/main.py
+++ api/main.py
@@ -5,6 +5,7 @@ dir_static_folder = "static/"
 
 app = Flask(__name__, template_folder=dir_template_folder, static_folder=dir_static_folder)
 
+
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
