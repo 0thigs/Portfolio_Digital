e7a7259b7f8e2ae55e330401fbc1ab36851f219a
0thigs <machinimavlogs@gmail.com>
2024-06-18 19:36:29 -0300

    💻script: add project description on project variable in main

--- api/main.py
+++ api/main.py
@@ -12,59 +12,68 @@ def homepage():
 
 @app.route("/projects")
 def projects():
-    academicProjects = [
+        
+        academicProjects = [
     {
         "title": "StarDust",
         "image": url_for('static', filename='/images/card(5).png'),
         "link": "https://github.com/0thigs/StarDust",
+        "description": "A mobile application for learning programming logic.",
     },
     {
         "title": "Smart Farm",
         "image": url_for('static', filename='/images/card(8).png'),
         "link": "https://github.com/ctrI-Alt-Del/smart-farming/",
+        "description": "A web application that helps monitoring a smart greenhouse.",
     },
     {
         "title": "Youtube Music Clone",
         "image": url_for('static', filename='/images/card(4).png'),
-        "link": "https://github.com/0thigs/Youtube_music"
+        "link": "https://github.com/0thigs/Youtube_music",
+        "description": "A Youtube music UI Clone to listen your favorite musics.",
     },
-]
+]   
     
-    personalProjects = [
+        personalProjects = [
     {
         "title": "Opportunity",
         "image": url_for('static', filename='/images/card(7).png'),
         "link": "https://github.com/0thigs/Opportunity",
+        "description": "A website that aims to provides information about general oportunities."
     },
     {
         "title": "Pokedéx",
         "image": url_for('static', filename='/images/card(6).png'),
         "link": "https://github.com/0thigs/Pokedex-App",
+        "description": "A Pokedex application to see your favorite pokemon stats."
     },
     {
         "title": "ChatApp",
         "image": url_for('static', filename='/images/card(1).png'),
         "link": "https://github.com/0thigs/ChatApp",
+        "description": "A web chat app used to meet new people."
     },
     {
         "title": "DevLinks",
         "image": url_for('static', filename='/images/card(2).png'),
         "link": "https://github.com/0thigs/DevLinks",
+        "description": "A social media links agragator."
     },
     {
         "title": "Github Finder",
         "image": url_for('static', filename='/images/card(3).png'),
         "link": "https://github.com/0thigs/Github-Profile-Finder",
+        "description": "A web app to search profiles on Github."
     },
       {
         "title": "Rock Papper Scissor",
         "image": url_for('static', filename='/images/card(9).png'),
         "link": "https://github.com/0thigs/Rock-Papper-Scissor",
+        "description": "A rock papper scissor web game to have a little fun."
     },
-
 ]
     
-    return render_template("projects.html", academicProjects=academicProjects ,personalProjects=personalProjects)
+        return render_template("projects.html", academicProjects=academicProjects ,personalProjects=personalProjects)
 
 @app.route("/contact")
 def contact():
