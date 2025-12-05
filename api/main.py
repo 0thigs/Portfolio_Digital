f0d45979ac12cfb812abad9c6051f0b7eb468f01
0thigs <machinimavlogs@gmail.com>
2025-12-05 00:47:04 -0300

    🧱projects: add new projects and update project descriptions and images

--- api/main.py
+++ api/main.py
@@ -15,6 +15,24 @@ def projects():
         
         academicProjects = [
     {
+        "title": "Gaia",
+        "image": url_for('static', filename='/images/11.png'),
+        "link": "https://github.com/CtrI-Alt-Del/gaia",
+        "description": "Monitoring system for low-cost meteorological stations with dashboards and educational components.",
+    },
+    {
+        "title": "Chronos",
+        "image": url_for('static', filename='/images/12.png'),
+        "link": "https://github.com/CtrI-Alt-Del/chronos",
+        "description": "Web application for online time tracking with many features.",
+    },
+    {
+        "title": "Stocker",
+        "image": url_for('static', filename='/images/14.png'),
+        "link": "https://github.com/CtrI-Alt-Del/stocker/",
+        "description": "Inventory management system to manipulate products while tracking quantities and item details.",
+    },
+    {
         "title": "StarDust",
         "image": url_for('static', filename='/images/card(5).png'),
         "link": "https://github.com/0thigs/StarDust",
@@ -24,17 +42,29 @@ def projects():
         "title": "Smart Farm",
         "image": url_for('static', filename='/images/card(8).png'),
         "link": "https://github.com/ctrI-Alt-Del/smart-farming/",
-        "description": "A web application that helps monitoring a smart greenhouse.",
+        "description": "A web application that helps monitor a smart greenhouse.",
     },
     {
         "title": "Youtube Music Clone",
         "image": url_for('static', filename='/images/card(4).png'),
         "link": "https://github.com/0thigs/Youtube_music",
-        "description": "A Youtube music UI Clone to listen your favorite musics.",
+        "description": "A YouTube Music UI clone for listening to your favorite music.",
     },
-]   
+]
     
         personalProjects = [
+    {
+        "title": "Cookiefy",
+        "image": url_for('static', filename='/images/10.png'),
+        "link": "https://github.com/0thigs/cookiefy",
+        "description": "Cookiefy is a mobile app for sharing and discovering culinary recipes, connecting food lovers through an intuitive experience.",
+    },
+    {
+        "title": "Meetle",
+        "image": url_for('static', filename='/images/13.png'),
+        "link": "#",
+        "description": "Meetle is a social platform focused on authentic conversations, fostering meaningful connections through shared interests.",
+    },
     {
         "title": "Opportunity",
         "image": url_for('static', filename='/images/card(7).png'),
